fa7455b099f5b8989d60c4b5a55ba36901206be9
DavyJohn <1139099003@qq.com>
2020-11-02 21:57:30 +0800

    添加本地缓存以及网络i请求优化+asycn

--- pages/cateforyx/index.js
+++ pages/cateforyx/index.js
@@ -1,12 +1,11 @@
 // pages/cateforyx/index.js
-
-import {request} from "../../request/index.js"
+import {request} from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime";
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-  
     //左侧的整体列表数据
     leftCateList:[],
     //右侧列表整体数据数据
@@ -15,6 +14,8 @@ Page({
     selOptions:0,
     //右侧子项数据
     rightItemList:[],
+    //设置右侧页面滚回顶部设置
+    srcollTop:0
   },
   // 申明一个全局变量
   Cates:[],
@@ -22,29 +23,57 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
+
   onLoad: function (options) {
-    this.getCates()
+    const cates =wx.getStorageSync('cates');
+    console.log(cates);
+    if (!cates) {
+      this.getCates()
+    } else {
+      if(Date.now() - cates.time>1000*60*60){
+        this.getCates()
+      }else{
+        this.Cates = cates.data,
+        this.setData({
+          leftCateList:this.Cates.map(v=>v.cat_name), //左边的item菜单
+          rightCateList:this.Cates.map(v=>v.children),//获取右边所有的数据
+          rightItemList:this.Cates[0].children
+      }
+    )
+      }
+    }
   },
-  getCates:function(){
-    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/categories"})
-    .then(result=>{
-      this.Cates=result.data.message
-      console.log(this.Cates)
-      this.setData({
-        leftCateList:this.Cates.map(v=>v.cat_name), //左边的item菜单
-        rightCateList:this.Cates.map(v=>v.children),//获取右边所有的数据
-        rightItemList:this.Cates[0].children
-      })
+ async getCates(){
+  const result = await request({url:"/categories"});
+  this.Cates = result,
+  wx.setStorageSync("cates", {time:Date.now(),data:result})
+  this.setData({
+    leftCateList:this.Cates.map(v=>v.cat_name), //左边的item菜单
+    rightCateList:this.Cates.map(v=>v.children),//获取右边所有的数据
+    rightItemList:this.Cates[0].children,
+    srcollTop:0
+  } )
+ },
+  // getCates:function(){
+  //   request({url:"/categories"})
+  //   .then(result=>{
+  //     this.Cates=result,
+  //     wx.setStorageSync('cates',{time:Date.now(),data:this.Cates}),
+  //     console.log(this.Cates)
+  //     this.setData({
+  //       leftCateList:this.Cates.map(v=>v.cat_name), //左边的item菜单
+  //       rightCateList:this.Cates.map(v=>v.children),//获取右边所有的数据
+  //       rightItemList:this.Cates[0].children,
+  //       srcollTop:0
+  //     })
    
-    })
-  },
+  //   })
+  // },
   onItemClick:function(e){
     this.setData({
       selOptions:e.currentTarget.dataset['index'],
-      rightItemList : this.Cates[this.data.selOptions].children
+      rightItemList : this.Cates[this.data.selOptions].children,
+      
     })
-   
-   
-    
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,50 +1,78 @@
 //index.js
 //获取应用实例
 const app = getApp()
-import {request} from "../../request/index.js"
+import {request} from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime";
 Page({
   data: {
     bannerList:[],
     cateList:[],
     floorList:[],
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     // 获取轮播图数据
-    this.getBanner(),
-    //获取首页分类数据
-    this.getCateList(),
+    console.log("开启获取轮播图");
+    const banner = wx.getStorageSync('bannerdata');
+    if(!banner){
+      console.log("没有缓存进行网络请求"),
+      this.getBanner()
+    }else{
+      console.log("有缓存进行过期判断");
+      if(Date.now()-banner.time>1000*60*60){
+        console.log("过期重新请求");
+        this.getBanner()
+      }else{
+        console.log("没过期直接获取缓存数据"+banner);
+        this.setData({
+          bannerList:banner.data
+        })
+      }
+     
+    }
+   
+    //获取首页分类数据（参照轮播图可以进行优化缓存）
+    this.getCateList(),  //进行网络优化async
     //获取楼层数据
     this.getFloorData()
   },
  // 获取轮播图数据
   getBanner:function(){
-    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"})
+    request({url:"/home/swiperdata"})
     .then(result => {
+      wx.setStorageSync("bannerdata", {time:Date.now(),data:result}),
+      console.log("输出轮播图信息"),
       this.setData({
-        bannerList:result.data.message
+        bannerList:result
       })
     }) 
   },
   //获取首页分类数据
-  getCateList:function(){
-    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"})
-    .then(result =>{
-      this.setData({
-        cateList:result.data.message
-      })
+  async getCateList(){
+    const result = await request({url:"/home/catitems"});
+    console.log(result);
+    //进行网络本地缓存
+    wx.setStorageSync('CateList', {time:Date.now(),data:result});
+    this.setData({
+      cateList:result
     })
+    // request({url:"/home/catitems"})
+    // .then(result =>{
+    //   this.setData({
+    //     cateList:result
+    //   })
+    // })
   },
 //获取楼层数据
   getFloorData:function(){
-    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/floordata"})
+    request({url:"/home/floordata"})
     .then(result=>{
       this.setData({
-        floorList:result.data.message
+        floorList:result
       })
     })
 }
